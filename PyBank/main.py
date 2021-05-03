2f819069249021484b62bd49fd0fe7f76bb650f1
uldom <muldom@gmail.com>
2021-05-03 17:38:46 -0500

    Work in progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,12 +5,28 @@ import csv
 # Specify the path for the csv file
 csvpath = os.path.join(r"D:\036_Rice\1_Homework_UD\Python_Challenge\PyBank\Resources\budget_data.csv")
 
-#Open the CSV file
+# Open the CSV file
 with open(csvpath) as csvfile:
     csvreader = csv.reader (csvfile,delimiter=",")
-
-# Loop
+    print(csvreader)
+    csv_header = next(csvreader,None)
+    # Declare variables
+    Months = 0
+    Total_amount=[]
+    
+    # Loop
     for row in csvreader:
-        print(row)
-print (type(row[1]))
+        months = months + 1
+        Total_amount.append (int(row[1]))
+    
+    
+#print(row)
+#print (type(row[1]))
 
+print ("Financial Analysis")
+print ("------------------")
+print (f"Total Months: {Months} months")
+print ("Total Amount:")
+print ("Average Change:")
+print ("Greatest Increase in Profits:")
+print ("Greatest Decrease in Profits:")
